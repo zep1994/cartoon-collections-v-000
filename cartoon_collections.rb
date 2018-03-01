7ab45f688e505a931a7ac68c7b8a55f679411537
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-03-01 23:57:12 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,8 +16,14 @@ def summon_captain_planet(planeteer_calls)
   array
 end
 
-def long_planeteer_calls()
-
+def long_planeteer_calls(array)
+ i = 0 
+  if  array.any? {|i| i.length > 4}
+    return true
+  else 
+    return false
+  i = i + 1
+  end 
 end
 
 def find_the_cheese()

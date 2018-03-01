28f59e0798cdd42ab828c550f0328619273b9129
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-03-01 23:52:46 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,13 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet()
- 
+  array = []
+  i = 0 
+  while i < array.length 
+    nu_array << array[i].capitalize + "!"
+    i += 1 
+  end 
+  nu_array
 end
 
 def long_planeteer_calls()

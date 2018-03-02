9be9116deabbfa324206bd2fc49ab9e5d4c103d4
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-03-02 00:00:16 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,7 +26,7 @@ def long_planeteer_calls(array)
   end 
 end
 
-def find_the_cheese()
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
    array.find do |cheese|
